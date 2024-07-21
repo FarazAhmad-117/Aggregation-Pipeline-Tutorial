928ea258210f9553e4cc6394a46c308685a04e71
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-21 18:59:07 +0500

    my version done

---
+++ Biggners - Starting/Find top 5 most liked fruits among users.mongodb.js
@@ -0,0 +1,18 @@
+
+
+// So here is what I am supposed to do find top five fruits liked by out users
+
+// My Way:
+
+use("aggregation");
+
+
+db.users.aggregate([
+    {
+        $group: {
+            _id: "$favoriteFruit",
+            count:{$sum:1} // Adding 1 for each entry
+        }
+    }
+])
+
