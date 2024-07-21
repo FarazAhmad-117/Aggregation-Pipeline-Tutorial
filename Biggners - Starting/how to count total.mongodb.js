2eb9442cd632052798501de88aef73b57f62df99
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-21 17:41:25 +0500

    count learning

---
+++ Biggners - Starting/how to count total.mongodb.js
@@ -0,0 +1,32 @@
+
+
+// How to find total users count
+
+use("aggregation");
+
+
+// db.authors.count() // One way!
+
+// db.users.aggregate([
+//     // Stage 1:
+//     {
+//         $match: {
+//             isActive:false
+//         }
+//     },
+//     {
+//         $count: 'nonActiveUsers'
+//     }
+// ]);
+
+
+
+
+
+
+
+
+
+
+
+
